beae254a058a44b50de8463b49c17c3ed5ff963e
William Harrington <kb0iic@gmail.com>
2017-03-17 14:45:22 -0500

    Add project files.

---
+++ BouncingBall2/Program.cs
@@ -0,0 +1,23 @@
+﻿// http://www.sourcecodester.com/tutorials/c/7549/bouncing-ball-c.html
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBall2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
